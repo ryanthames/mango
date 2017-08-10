bc060165ed24117ec92c245c05b016fd612cd11f
Ryan Thames <ryanthames@gmail.com>
2017-08-09 21:13:07 -0500

    WIP

--- test/mango_web/acceptance/homepage_test.exs
+++ test/mango_web/acceptance/homepage_test.exs
@@ -4,8 +4,23 @@ defmodule MangoWeb.HomepageTest do
 
   hound_session()
 
-  test "presence of featured products" do
+  test "presence of seasonal products" do
+    ## GIVEN ##
+    # There are two products Apple and Tomato price 100 and 50 respectively
+    # Where Apple being the only seasonal product
+
     navigate_to("/")
-    assert page_source() =~ "Seasonal products" 
+    page_title = find_element(:css, ".page-title")
+      |> visible_text()
+    assert page_title == "Seasonal Products"
+
+    product = find_element(:css, ".product")
+    product_name = find_within_element(product, :css, ".product-name")
+    product_price = find_within_element(product, :css, ".product-price")
+
+    assert product_name == "Apple"
+    assert product_price == "100"
+
+    refute page_source() =~ "Tomato"
   end
 end
